37c775cd9842b816a555c3f12963288bad468e47
taku-0728 <taku8270@gmail.com>
2021-08-02 22:07:53 +0900

    [add] 6章で書いたコードを追加

---
+++ 6章/bar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="bar">
+        <h1 class="header">Barコンポーネント</h1><p>これはBarコンポーネントです。</p>
+        <foo/>
+    </div>
+</template>
+
+<script>
+import Foo from './foo'
+export default {
+    components: { Foo }
+}
+</script>
+
+<style scoped>
+.bar { border: 1px solid red; margin: 4px; padding: 4px; }
+.header { font-size: 125%; }
+</style>
---
+++ 6章/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <p class="message">こんにちは！</p>
+</template>
+
+<script>
+export default {
+    created() {
+        console.log('css modules: $style', this.$style);
+    },
+}
+</script>
+
+<style module>
+.message {color: #42b983;}
+</style>
